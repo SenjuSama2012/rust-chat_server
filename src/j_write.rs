cad783a930528bcda9cbcb7ff68e7fa02de7cad1
Jahmal Senju Camp <senjusama2012@gmail.com>
2022-03-12 11:45:01 -0600

    Convert Input to A Rust Type Using Serde

--- src/j_write.rs
+++ src/j_write.rs
@@ -1,19 +1,18 @@
-#! [allow(unused_imports)]
+#![allow(unused_imports)]
 
 use futures::future::Future;
 use futures::try_ready;
 use serde::Serialize;
 use tokio::prelude::*;
 
-
-pub struct JWrite<S,W> {
-    in_s:S,
-    out_w:W,
+pub struct JWrite<S, W> {
+    in_s: S,
+    out_w: W,
     buff: Option<Vec<u8>>,
 }
 
-impl <S,W> JWrite <S,W> {
-    pub fn new(in_s:S, out_w:W) -> Self {
+impl<S, W> JWrite<S, W> {
+    pub fn new(in_s: S, out_w: W) -> Self {
         JWrite {
             in_s,
             out_w,
@@ -23,34 +22,34 @@ impl <S,W> JWrite <S,W> {
 }
 
 impl<S, I, W> Future for JWrite<S, W>
-    where 
-    S:Stream<Item = I, Error = ()>,
+where
+    S: Stream<Item = I, Error = ()>,
     I: Serialize,
     W: AsyncWrite,
 {
     type Item = ();
     type Error = ();
-    fn poll(&mut self) -> Result <Async<Self::Item>, Self::Error> {
-            loop {
-                match self.buff {
-                    None => {
-                        let i = match try_ready! {self.in_s.poll()} {
-                            Some(v) => v,
-                            None => return Ok(Async::Ready(())),
-                        };
-                        self.buff = serde_json::to_string(&i)
-                            .map(|v| v.as_bytes().to_vec())
-                            .ok();
+    fn poll(&mut self) -> Result<Async<Self::Item>, Self::Error> {
+        loop {
+            match self.buff {
+                None => {
+                    let i = match try_ready! {self.in_s.poll()} {
+                        Some(v) => v,
+                        None => return Ok(Async::Ready(())),
+                    };
+                    self.buff = serde_json::to_string(&i)
+                        .map(|v| v.as_bytes().to_vec())
+                        .ok();
+                }
+                Some(ref mut v) => {
+                    let n = try_ready! {self.out_w.poll_write(v).map_err(|e| println!("Could not write out"))};
+                    if n == v.len() {
+                        self.buff = None;
+                    } else {
+                        self.buff = Some(v.split_off(n));
                     }
-                    Some (ref mut v) => {
-                        let n = try_ready! {self.out_w.poll_write(v).map_err(|e| println!("Could not write out"))};
-                        if n == v.len(){
-                            self.buff = None;
-                        } else {
-                            self.buff = Some(v.split_off(n));
-                        }
-                    }
-                }            
+                }
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,47 @@
-#! [allow(dead_code)]
-#! [allow(unused_variables)]
-#! [allow(unused_assignments)]
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_assignments)]
 
+use serde_derive::*;
 use tokio::net::TcpListener;
 use tokio::prelude::*;
 
-
 mod j_read;
 mod j_write;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Message {
+    name: String,
+    tx: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ServerMessage {
+    mess: Option<Message>,
+    since: Option<usize>,
+}
 
 fn main() {
     let addr = "127.0.0.1:5500".parse().unwrap();
     let lis = TcpListener::bind(&addr).expect("Could not bind address");
 
-        let fut = lis.incoming().for_each(|sock| {
+    let fut = lis
+        .incoming()
+        .for_each(|sock| {
             let (sock_r, sock_w) = sock.split();
-            let rd = j_read::JRead::new(sock_r).for_each(|s| {
-                println!("Recieved: {}", s);
-            Ok(())
-            }).map_err(|_|());
+            let rd = j_read::JRead::new(sock_r)
+                .for_each(|s| {
+                    let v: ServerMessage = serde_json::from_str(&s)?;
+                    println!("Recieved: {:?}", v);
+                    Ok(())
+                })
+                .map_err(|_| ());
             tokio::spawn(rd);
             Ok(())
-        }).map_err(|e| println!("Listening Err :{:?}", e ));
+        })
+        .map_err(|e| println!("Listening Err :{:?}", e));
 
-        tokio::run(fut);
+    tokio::run(fut);
 
-        println!("Hello World");
+    println!("Hello World");
 }
-
